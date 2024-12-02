15a2b06d45eeca10e16ee695dd22f2d577b9ca61
Scott Joseph <scott.joseph@gmail.com>
2024-12-02 15:33:34 -0500

    Create module1-data-types.py
    git commit 'datatypes' ...

---
+++ module1-data-types.py
@@ -0,0 +1,16 @@
+greeting = 'Hello, friend!' #string
+
+age = 35                    #integer
+
+speed1 = 4.5                #floats
+speed2 = 4.0                #floats
+speed3 = 4.                 #floats
+speed = 4                   #floats
+
+am_i_ugly = True            #boolean
+am_i_ugly = False           #boolean
+
+
+#In programming 
+#True = 1
+#False = 0
