e94a398cbd4c31aacb7b0b052bb227a14e644f9c
Scott Joseph <scott.joseph@gmail.com>
2024-12-03 16:31:57 -0500

    Create module1-reassigning-values.py
    git commit -m 'reassign values

---
+++ module1-reassigning-values.py
@@ -0,0 +1,25 @@
+age = 59
+print(age)
+
+age = 59
+new_age = age + 5
+print(new_age)
+
+age = 28
+age = age + 7
+print(age)
+
+age = 28
+age += 7
+print(age)
+
+text = 'hokus' + 'pokus'
+print (text)
+
+print('hokus' * 5)
+
+print(23+5)
+
+print('23' + '5')
+
+
