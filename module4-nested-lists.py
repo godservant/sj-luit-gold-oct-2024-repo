07664f45bfa1840d09a67edd7284d53e94afa066
Scott Joseph <scott.joseph@gmail.com>
2024-12-17 18:16:28 -0500

    Create Module4-list-comp
    git commit -m 'module4-list-comp

---
+++ module4-nested-lists.py
@@ -0,0 +1,12 @@
+#cells = [['A1', 'A2', 'A3'], ['B1', 'B2', 'B3']]
+#for x in cells:
+#    print('Element:', x)
+    
+    
+cells = [['A1', 'A2', 'A3'], ['B1', 'B2', 'B3']]
+for x in cells:
+    for y in x:
+        print('Element:',  y)
+        
+        
+    
