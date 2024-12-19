cd2e9d42189e1a73a4fcf297bb5e47215a7aa237
Scott Joseph <scott.joseph@gmail.com>
2024-12-19 18:52:05 -0500

    Create module4-tuple-operations.py
    git commit -a -m "module4 tuple operations"

---
+++ module4-tuple-operations.py
@@ -0,0 +1,11 @@
+user_data = ('John', 'American', 1964)
+if 'American' in user_data:
+    print('This person comes from the US!')
+    
+user_data = ('John', 'American', 1964)
+for element in user_data:
+    print(element)
+    
+user_data = ('John', 'American', 1964) + ('teacher', 'male')
+print(user_data)
+
