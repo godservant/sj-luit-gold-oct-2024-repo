35275472b36babb90ae090b9f579480806badf09
Scott Joseph <scott.joseph@gmail.com>
2024-12-29 13:21:02 -0500

    Create module5-assertions.py
    git commit -m -a "learning about assertions in Python"

---
+++ module5-assertions.py
@@ -0,0 +1,7 @@
+def calculate_inverse(number):
+    assert (number !=0), 'Got 0 as number!'
+    return 1/number
+
+
+# Assertions are used for debugging & troubleshoting
+# For documentation as well
